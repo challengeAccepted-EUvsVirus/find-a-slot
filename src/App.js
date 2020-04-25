4f8b0ee5e182968ea18ae84c7f64be705c8f0f52
Mario Arranz <mario@paddle.com>
2020-04-25 17:32:32 +0100

    cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Form, Col, Container, Row, Button } from 'react-bootstrap';
+import Moment from 'moment';
+import { Form, Col, Container, Row, Button, Card } from 'react-bootstrap';
 import './App.css';
 import { backendUrl } from './utils';
 
@@ -9,9 +10,10 @@ class App extends Component {
     super(props);
 
     this.state = {
+      country_code: 'en',
       isLoading: false,
       formData: {
-        postcode: "XXXXX",
+        "post_code": "28023"
       },
       result: ""
     };
@@ -30,16 +32,23 @@ class App extends Component {
   handleSubmitClick = (event) => {
     const formData = this.state.formData;
     this.setState({ isLoading: true });
-    fetch(backendUrl)
-    // fetch(backendUrl,
-    //   {
-    //     headers: {
-    //       'Accept': 'application/json',
-    //       'Content-Type': 'application/json'
-    //     },
-    //     method: 'POST',
-    //     body: JSON.stringify(formData)
-    //   })
+    fetch("https://ipapi.co/json")
+    .then(ipresponse => ipresponse.json())
+    .then(ipresponse => {
+      this.setState({
+        country_code: ipresponse.languages.split(",")[0],
+      });
+      formData.country = this.state.country_code
+      fetch(backendUrl)
+      // fetch(backendUrl,
+      //   {
+      //     headers: {
+      //       'Accept': 'application/json',
+      //       'Content-Type': 'application/json'
+      //     },
+      //     method: 'POST',
+      //     body: JSON.stringify(formData)
+      //   })
       .then(response => response.json())
       .then(response => {
         this.setState({
@@ -49,7 +58,9 @@ class App extends Component {
       })
       .catch(error => {  
         console.log('Request failure: ', error);  
-      });
+      })
+
+    });
   }
 
   handleClearClick = (event) => {
@@ -59,6 +70,7 @@ class App extends Component {
   render() {
     const isLoading = this.state.isLoading;
     const result = this.state.result;
+    Moment.locale(this.state.country_code);
     return (
       <Container>
         <div>
@@ -93,9 +105,65 @@ class App extends Component {
           </Form>
           {result === "" ? null :
           <Row>
-            {result.map((element, idx) => (
+            {[
+              {
+                  "id":1234,
+                  "name":"Drusifour",
+                  "image_url":"https://upload.wikimedia.org/wikipedia/commons/5/5b/Carrefour_logo.svg",
+                  "slots":[
+                    {
+                        "datetime_start":"2015-08-05T08:40:51.620Z",
+                        "datetime_end":"2016-08-05T08:40:51.620Z"
+                    },
+                    {
+                        "datetime_start":"2020-08-05T08:40:51.620Z",
+                        "datetime_end":"2021-08-05T08:40:51.620Z"
+                    }
+                  ]
+              },{
+                "id":1234,
+                "name":"Drusifour",
+                "image_url":"https://upload.wikimedia.org/wikipedia/commons/5/5b/Carrefour_logo.svg",
+                "slots":[
+                  {
+                      "datetime_start":"2015-08-05T08:40:51.620Z",
+                      "datetime_end":"2016-08-05T08:40:51.620Z"
+                  },
+                  {
+                      "datetime_start":"2020-08-05T08:40:51.620Z",
+                      "datetime_end":"2021-08-05T08:40:51.620Z"
+                  }
+                ]
+            },
+            {
+              "id":1234,
+              "name":"Drusifour",
+              "image_url":"https://upload.wikimedia.org/wikipedia/commons/5/5b/Carrefour_logo.svg",
+              "slots":[
+                {
+                    "datetime_start":"2015-08-05T08:40:51.620Z",
+                    "datetime_end":"2016-08-05T08:40:51.620Z"
+                },
+                {
+                    "datetime_start":"2020-08-05T08:40:51.620Z",
+                    "datetime_end":"2021-08-05T08:40:51.620Z"
+                }
+              ]
+          }
+            ].map((element, idx) => (
               <Col key={idx} className="result-container">
-                <h5 id="result">{element.name} {element.username}</h5>
+                <Card className="result">
+                  <Card.Img variant="top" src={element.image_url} />
+                  <Card.Body>
+                    <Card.Title>Drusifour</Card.Title>
+                    {element.slots.map((slot, idy) => (
+                      <Card.Text key={idy}>
+                        {Moment(slot.datetime_start).format('d MMM hh:mm')} - {Moment(slot.datetime_end).format('hh:mm')}
+                      </Card.Text>
+                     ))}
+                    <Button href={element.url} variant="primary">Visit</Button>
+                  </Card.Body>
+                </Card>
               </Col>
             ))}
             </Row>
