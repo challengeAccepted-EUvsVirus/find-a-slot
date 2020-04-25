19a55daf3b2273af2b073452f44c23588a0d8205
Mario Arranz <mario@paddle.com>
2020-04-25 11:10:19 +0100

    show mock data

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,7 @@
 // for backend
 export const backendUrl = 
   process.env.NODE_ENV == 'production'
-    ? 'CHANGE_ME/find/'
+    ? 'http://jsonplaceholder.typicode.com/users'
+    // ? 'CHANGE_ME/find/'
     : 'http://jsonplaceholder.typicode.com/users'
     // : 'http://127.0.0.1:5000/find'
