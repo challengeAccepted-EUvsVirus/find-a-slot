97ac194018c1f3b46312b9386a17a8dc09b47c2f
Mario Arranz <mario@paddle.com>
2020-04-25 11:13:09 +0100

    https fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@
 // for backend
 export const backendUrl = 
   process.env.NODE_ENV == 'production'
-    ? 'http://jsonplaceholder.typicode.com/users'
+    ? 'https://jsonplaceholder.typicode.com/users'
     // ? 'CHANGE_ME/find/'
-    : 'http://jsonplaceholder.typicode.com/users'
+    : 'https://jsonplaceholder.typicode.com/users'
     // : 'http://127.0.0.1:5000/find'
